e265265bed96846e0b3cab401afe196292216ce2
Sergi Pasoev <s.pasoev@gmail.com>
2017-10-26 22:27:29 +0400

    vim

--- emacs/init.el
+++ emacs/init.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
  '(inhibit-startup-buffer-menu nil)
  '(inhibit-startup-screen t))
 (custom-set-faces
@@ -19,5 +20,22 @@
 
 (find-file "~/org/todo.org")
 
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+	;; This would override `fill-column' if it's an integer.
+	(emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 ;; Modes
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
+(add-hook 'org-mode 'turn-on-auto-fill)
+
+(setq c-default-style "linux" c-basic-offset 4)
+;; (setq-default c-basic-offset 4)
+
+(setq c-default-style "linux" c-basic-offset 4)
