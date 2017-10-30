92650ffff0234f8d0cae04679b5c18016cf3d884
Sergi Pasoev <s.pasoev@gmail.com>
2017-10-30 15:21:12 +0400

    org mode configuration

--- emacs/init.el
+++ emacs/init.el
@@ -12,6 +12,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+ '(custom-enabled-themes (quote (tango-dark)))
  '(inhibit-default-init t)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
@@ -58,7 +61,7 @@
   %u" "~/org/todo.org" "Tasks")
      (110 "* %u %?" "~/org/notes.org" "Notes"))))
  '(org-reverse-note-order t)
- '(package-selected-packages (quote ("racket-mode" racket-mode)))
+ '(package-selected-packages (quote (clojure-mode "racket-mode" racket-mode)))
  '(remember-annotation-functions (quote (org-remember-annotation)))
  '(remember-handler-functions (quote (org-remember-handler)))
  '(tool-bar-mode nil)
@@ -81,6 +84,7 @@
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 
 (define-key mode-specific-map [?a] 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 
 (eval-after-load "org"
   '(progn
@@ -106,6 +110,24 @@
      (define-key org-agenda-mode-map "\C-p" 'previous-line)
      (define-key org-agenda-keymap "\C-p" 'previous-line)))
 
+(setq org-capture-templates
+      (quote (("t" "todo" entry (file "~/org/refile.org")
+               "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("r" "respond" entry (file "~/org/refile.org")
+               "* NEXT Respond to %:from on %:subject\nSCHEDULED: %t\n%U\n%a\n" :clock-in t :clock-resume t :immediate-finish t)
+              ("n" "note" entry (file "~/org/refile.org")
+               "* %? :NOTE:\n%U\n%a\n" :clock-in t :clock-resume t)
+              ("j" "Journal" entry (file+datetree "~/org/todo.org")
+               "* %?\n%U\n" :clock-in t :clock-resume t)
+              ("w" "org-protocol" entry (file "~/org/refile.org")
+               "* TODO Review %c\n%U\n" :immediate-finish t)
+              ("m" "Meeting" entry (file "~/org/refile.org")
+               "* MEETING with %? :MEETING:\n%U" :clock-in t :clock-resume t)
+              ("p" "Phone call" entry (file "~/org/refile.org")
+               "* PHONE %? :PHONE:\n%U" :clock-in t :clock-resume t)
+              ("h" "Habit" entry (file "~/org/refile.org")
+               "* NEXT %?\n%U\n%a\nSCHEDULED: %(format-time-string \"%<<%Y-%m-%d %a .+1d/3d>>\")\n:PROPERTIES:\n:STYLE: habit\n:REPEAT_TO_STATE: NEXT\n:END:\n"))))
+
 
 (add-hook 'remember-mode-hook 'org-remember-apply-template)
 
@@ -117,3 +139,21 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'narrow-to-page 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+
+
+(require 'package) ;; You might already have this line
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
+(when (< emacs-major-version 24)
+  ;; For important compatibility libraries like cl-lib
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
+(package-initialize) ;; You might already have this line
+
+;; clojure
+;; (package-refresh-contents)
+(unless (package-installed-p 'clojure-mode)
+   (package-install 'clojure-mode))
+
+
